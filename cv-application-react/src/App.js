7ab99c664b487179e10f02eaa8361b1bf30cea8a
kubraguness6 <kubragunes01@gmail.com>
2022-03-22 01:03:08 +0300

    initial

---
+++ cv-application-react/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePageComponent from './homePageComponent/HomePageComponent';
+
+function App() {
+  return (
+    <HomePageComponent></HomePageComponent>
+  );
+}
+
+export default App;
---
+++ cv-application-react/src/homePageComponent/HomePageComponent.jsx
@@ -0,0 +1,192 @@
+const HomePageComponent = () => {
+    return (
+        <div><div class="container">
+            <div class="main-body">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex flex-column align-items-center text-center">
+                                    <img src="https://avatars.githubusercontent.com/u/93152092?v=4" alt="Admin" class="rounded-circle p-1 bg-primary" width="110" />
+                                    <div class="mt-3">
+                                        <h4>Kübra Güneş</h4>
+                                        <p class="text-secondary mb-1">Siber Güvenlik Analisti</p>
+                                        <p class="text-muted font-size-sm">Edirne/Merkez, Türkiye</p>
+                                    </div>
+                                </div>
+                                <hr class="my-4" />
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe me-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
+                                        <span class="text-secondary">www.kubragunes.net</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github me-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
+                                        <span class="text-secondary">kubragunes</span>
+                                    </li>
+
+                                </ul>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h3 class="mb-0">Eğitim</h3>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h6 class="mb-0">Lise</h6>
+                                        <span class="text-secondary">Turhan Tayan Anadolu Lisesi(2015-2019)</span>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h6 class="mb-0">Lisans</h6>
+                                        <span class="text-secondary">Trakya Üniversitesi</span>
+                                        <span style={{ marginLeft: "90px" }} class="text-secondary">Bilgisayar Mühendisliği (2019-2023)</span>
+
+                                    </li>
+                                </ul>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h3 class="mb-0">İş Tecrübesi</h3>
+                                    </li>
+                                    <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <h6 class="mb-0">-</h6>
+                                        <span class="text-secondary">-</span>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Tam Ad</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <h6 class="text-secondary" >Kübra Güneş</h6>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">E-posta</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <h6 class="text-secondary" >kubragunes@gmail.com</h6>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Telefon</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <h6 class="text-secondary" >(224) 224-2442</h6>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Mobil</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <h6 class="text-secondary" >(538) 538-5338 </h6>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Adres</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <h6 class="text-secondary" >Bursa, Türkiye</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1>Özgeçmiş</h1>
+                                        <h6>
+                                            06.01.2001 tarihinde Bursa'da doğdum. Lise eğitimimi Turhan Tayan Anadolu Lisesinde bitirdim. 2019 yılında Trakya Üniversitesi Bilgisayar Mühendisliği bölümüne başladım. Okulda yazılım ve donanımsal beceriler kazandım. Üniveriste ikinci sınıfta siber güvenlik alanına ilgi duyduğumu keşfederek bu alana yöneldim ve kendimi geliştirmeye başladım.
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h1>Sertifikalar</h1>
+                                        <h1>Introduction to Cybersecurity</h1>
+                                        <h6>
+                                            Siber güvenlik alanı ile ilgili temel becerileri kazandım.
+                                        </h6>
+                                        <h1>NDG Linux</h1>
+                                        <h6>
+                                            Linux işletim sistemini giriş seviyesinde öğrendim.
+                                        </h6>
+                                        <h1>Ağlara Giriş</h1>
+                                        <h6>
+                                            Bilgisayar ağlarına hakimiyet sağladım.
+                                        </h6>
+                                        <h1>CyberOps Associate</h1>
+                                        <h6>
+                                            Blue Team olarak ilk adımlarımı attım.
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="d-flex align-items-center mb-3">Geliştirici Yetenekleri</h5>
+                                        <p>Ağ Güvenliği</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-primary" role="progressbar" style={{ width: "70%" }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <p>Ağ Tasarımı</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-danger" role="progressbar" style={{ width: "85%" }} aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <p>Linux</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-success" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="d-flex align-items-center mb-3">İletişim Yetenekleri</h5>
+                                        <p>Araştırma</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-primary" role="progressbar" style={{ width: "90%" }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <p>İletişim</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-danger" role="progressbar" style={{ width: "95%" }} aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <p>Takım Çalışması</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-success" role="progressbar" style={{ width: "70%" }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <p>Analitik Düşünce</p>
+                                        <div class="progress mb-3" style={{ height: "5px" }}>
+                                            <div class="progress-bar bg-success" role="progressbar" style={{ width: "85%" }} aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div></div>
+    )
+}
+export default HomePageComponent;
